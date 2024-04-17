0e47bd147288b33288dac6ecbdda00f1a97de8fc
Fadi <fadishadow@hotmail.com>
2024-04-17 12:02:05 +0200

    Created Model ParkedVehicle

--- Controllers/GarageController.cs
+++ Controllers/GarageController.cs
@@ -4,11 +4,11 @@ using GarageMVC.Models;
 
 namespace GarageMVC.Controllers;
 
-public class HomeController : Controller
+public class GarageController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<GarageController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public GarageController(ILogger<GarageController> logger)
     {
         _logger = logger;
     }
---
+++ Models/ParkedVehicle.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GarageMVC.Models
+{
+    public class ParkedVehicle
+    {
+
+        public int Id { get; set; }
+
+        public string VehicleType { get; set; }
+
+        [Required(ErrorMessage = "Register Number is required!")]
+        public string RegNumber { get; set; }
+
+        public string Color { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int NumberOfWheels { get; set; }
+
+        [Required]
+        public DateTime ParkingTime { get; set; }
+
+        [Required]
+        public bool IsParked { get; set; }
+
+
+
+
+    }
+}
